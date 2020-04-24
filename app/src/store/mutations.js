2b211c19fedb1f386611f7bfb972bdaeef6f1901
Thaneshwor <thaneshwor100@gmail.com>
2020-04-24 06:10:34 +0545

    Add constants

---
+++ app/src/store/mutations.js
@@ -0,0 +1,23 @@
+export const MOVIES_LOADING = 'MOVIES_LOADING';
+export const AUTHENTICATING = 'AUTHENTICATING';
+export const AUTHENTICATED = 'AUTHENTICATED';
+export const SET_STATE = 'SET_STATE';
+
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+export const FETCH_MOVIES = 'FETCH_MOVIE';
+export const FETCH_SERIES = 'FETCH_SERIES';
+export const FETCH_FEV_MOVSER = 'FETCH_FEV_MOVSER';
+export const FETCH_FEV_MOVIES = 'FETCH_FEV_MOVIES';
+export const FETCH_FEV_SERIES = 'FETCH_FEV_SERIES';
+
+export const SET_FEV_MOVSER = 'SET_FEV_MOVSER';
