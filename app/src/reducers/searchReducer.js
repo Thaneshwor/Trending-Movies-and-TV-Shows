6d75b7e762c6ae6d18426741b9db131ef2c2ada8
Thaneshwor <thaneshwor100@gmail.com>
2020-04-24 14:22:44 +0545

    Add Reducer to fetch series and movie data from api

--- app/src/reducers/searchReducer.js
+++ app/src/reducers/searchReducer.js
@@ -1,25 +1,40 @@
-import { FETCH_MOVIES, FETCH_SERIES } from '../store/mutations';
+import { FETCH_MOVIES, FETCH_SERIES, MOVIES_LOADING, SERIES_LOADING } from '../store/mutations';
 
 const initialState = {
     movies: [],
     series: [],
+    movies_loading: true,
+    series_loading: true,
 }
 
 export default function (state = initialState, action) {
 
     switch (action.type) {
         case FETCH_MOVIES:
+
             return ({
                 ...state,
-                text: action.payload,
-                loading: false,
+                movies: action.payload.results, ...state.movies,
+                movies_loading: false
             });
 
         case FETCH_SERIES:
             return ({
                 ...state,
-                text: action.payload,
-                loading: false,
+                series: action.payload.results, ...state.series,
+                series_loading: false
+            });
+
+        case MOVIES_LOADING:
+            return ({
+                ...state,
+                movies_loading: true
+            });
+
+        case SERIES_LOADING:
+            return ({
+                ...state,
+                series_loading: true
             });
 
         default:
