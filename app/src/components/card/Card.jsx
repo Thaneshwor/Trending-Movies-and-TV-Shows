9f1cdba4f4f3a20cfbb93248fab0c2dcda3cc7a6
Thaneshwor <thaneshwor100@gmail.com>
2020-04-24 14:24:49 +0545

    Align card image and fav icon

--- app/src/components/card/Card.jsx
+++ app/src/components/card/Card.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Img from '../../assets/img/rock.jpeg';
+// import { getImageUrl, getImdbLink, getYoutubeLink } from '../../utils/utils';
 import love from '../../assets/img/love.png';
 import nolove from '../../assets/img/love1.jpeg';
 import './card.css';
@@ -10,30 +11,39 @@ class Card extends Component {
         super(props)
 
         this.state = {
-            favourite: false
+            favourite: true
         }
     }
 
+    changeFavImg = () => {
+        let val = this.state.favourite;
+        console.log(val, ' this is the real value-------')
+        this.setState({
+            favourite: !this.state.favourite
+        })
+    }
     render() {
         return (
             <div>
                 <div className='card'>
                     <div className='img-card'>
-                        <img src={Img} alt='poster' />
+                        <div className='img-card-box'>
+                            <img src={`http://image.tmdb.org/t/p/w185//${this.props.poster_path}`} alt='poster' />
+                        </div>
                     </div>
 
                     <div className='desc-movie'>
                         <div className='rating-container'>
                             <div className='rating'>
-                                <span className='font-20'>8.5</span><span className='font-15 secondary-text-color'>/10</span>
+                                <span className='font-20'>{this.props.vote_average}</span><span className='font-15 secondary-text-color'>/10</span>
                             </div>
                             <div className='fav'>
-                                <img src={this.state.favourite ? nolove : love} alt='love' className='favourite' onMouseDown={this.changeFavourite} />
+                                <button className='btn-fav' onClick={this.changeFavImg}><img src={this.state.favourite ? nolove : love} alt='love' className='favourite' onMouseDown={this.changeFavourite} /></button>
                             </div>
                         </div>
 
                         <div className='mg-top-10'>
-                            <a href='#' className='font-20 primary-text-color mg-r-10'>The Last Dance</a>
+                            <a href='#' className='font-20 primary-text-color mg-r-10'>{this.props.name}</a>
                             <span className='font-12 secondary-text-color'>Action Movie</span>
                         </div>
                         <div className='secondary-text-color mg-top-10'>Playing on Netflix</div>
