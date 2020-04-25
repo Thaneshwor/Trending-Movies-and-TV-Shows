bef2b6800f34a0ecfe1f1af4f1fd81af2905b282
Thaneshwor <thaneshwor100@gmail.com>
2020-04-25 17:53:05 +0545

    Add movie and series component

---
+++ app/src/components/card/MovieCard.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Img from '../../assets/img/rock.jpeg';
+// import { getImageUrl, getImdbLink, getYoutubeLink } from '../../utils/utils';
+import love from '../../assets/img/love.png';
+import nolove from '../../assets/img/love1.jpeg';
+import './card.css';
+
+class MovieCard extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            favourite: true
+        }
+    }
+
+    changeFavImg = () => {
+        let val = this.state.favourite;
+        this.setState({
+            favourite: !this.state.favourite
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className='card'>
+                    <div className='img-card'>
+                        <div className='img-card-box'>
+                            <img src={`http://image.tmdb.org/t/p/w185//${this.props.movies.poster_path}`} alt='poster' />
+                        </div>
+                    </div>
+
+                    <div className='desc-movie'>
+                        <div className='rating-container'>
+                            <div className='rating'>
+                                <span className='font-20'>{this.props.movies.vote_average}</span><span className='font-15 secondary-text-color'>/10</span>
+                            </div>
+                            <div className='fav'>
+                                <button className='btn-fav' onClick={this.changeFavImg}><img src={this.state.favourite ? nolove : love} alt='love' className='favourite' onMouseDown={this.changeFavourite} /></button>
+                            </div>
+                        </div>
+
+                        <div className='mg-top-10'>
+                            <a href='#' className='font-20 primary-text-color mg-r-10'>{this.props.movies.name}</a>
+                            <span className='font-12 secondary-text-color'>Action Movie</span>
+                        </div>
+                        <div className='secondary-text-color mg-top-10'>Playing on Netflix</div>
+                        <div className='mg-top-10'><a href='#' className='primary-text-color'>Watch Trailer</a></div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MovieCard;
