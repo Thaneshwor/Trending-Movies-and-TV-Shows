8fbf40356dcc5e28eaed687cc7d75be1f89ffaa2
Thaneshwor <thaneshwor100@gmail.com>
2020-04-27 09:43:39 +0545

    Display dynamically movies fetched from database and api

--- app/src/components/card/MovieCard.jsx
+++ app/src/components/card/MovieCard.jsx
@@ -1,48 +1,76 @@
 import React, { Component } from 'react';
-import Img from '../../assets/img/rock.jpeg';
-// import { getImageUrl, getImdbLink, getYoutubeLink } from '../../utils/utils';
+import { connect } from 'react-redux';
 import love from '../../assets/img/love.png';
 import nolove from '../../assets/img/love1.jpeg';
 import './card.css';
-
+import { delFevMovSer, setFevMovSers } from '../../actions/favAction';
+import { DELETE_FAV_MOVSER, SET_FEV_MOVSER } from '../../store/mutations';
 class MovieCard extends Component {
 
     constructor(props) {
         super(props)
 
         this.state = {
-            favourite: true
+            favourite: false,
+
+        }
+    }
+
+    componentDidMount = () => {
+        if (this.props.isFavorite) {
+            this.setState({
+                favourite: true
+            })
         }
     }
 
     changeFavImg = () => {
-        let val = this.state.favourite;
+
         this.setState({
             favourite: !this.state.favourite
         })
+
+        if (!this.state.favourite) {
+            this.props.movies.isMovie = true;
+            this.props.setFevMovSers(this.props.movies);
+        }
     }
+
     render() {
+        let imgUrl = '';
+        let avg_voat = '';
+        let title = '';
+        if (!this.props.isFavorite) {
+            imgUrl = `http://image.tmdb.org/t/p/w185//${this.props.movies.poster_path}`;
+            avg_voat = this.props.movies.vote_average;
+            title = this.props.movies.title;
+        } else {
+            imgUrl = `http://image.tmdb.org/t/p/w185//${this.props.movies.movies_info.poster_path}`;
+            avg_voat = this.props.movies.movies_info.vote_average;
+            title = this.props.movies.movies_info.title;
+        }
+
         return (
             <div>
                 <div className='card'>
                     <div className='img-card'>
                         <div className='img-card-box'>
-                            <img src={`http://image.tmdb.org/t/p/w185//${this.props.movies.poster_path}`} alt='poster' />
+                            <img src={imgUrl} alt='poster' />
                         </div>
                     </div>
 
                     <div className='desc-movie'>
                         <div className='rating-container'>
                             <div className='rating'>
-                                <span className='font-20'>{this.props.movies.vote_average}</span><span className='font-15 secondary-text-color'>/10</span>
+                                <span className='font-20'>{avg_voat}</span><span className='font-15 secondary-text-color'>/10</span>
                             </div>
                             <div className='fav'>
-                                <button className='btn-fav' onClick={this.changeFavImg}><img src={this.state.favourite ? nolove : love} alt='love' className='favourite' onMouseDown={this.changeFavourite} /></button>
+                                <button className='btn-fav' onClick={this.changeFavImg}><img src={this.state.favourite ? love : nolove} alt='love' className='favourite' onMouseDown={this.changeFavourite} /></button>
                             </div>
                         </div>
 
                         <div className='mg-top-10'>
-                            <a href='#' className='font-20 primary-text-color mg-r-10'>{this.props.movies.name}</a>
+                            <a href='#' className='font-20 primary-text-color mg-r-10'>{title}</a>
                             <span className='font-12 secondary-text-color'>Action Movie</span>
                         </div>
                         <div className='secondary-text-color mg-top-10'>Playing on Netflix</div>
@@ -55,4 +83,9 @@ class MovieCard extends Component {
     }
 }
 
-export default MovieCard;
+const mapStateToProps = (state) => {
+    return {
+        favs: state.favMovieSers,
+    }
+}
+export default connect(mapStateToProps, { setFevMovSers, delFevMovSer })(MovieCard);
